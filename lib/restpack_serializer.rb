95d0fbd565ef9444708753f76ae995d52a9477c3
Campbell Allen <campbell.allen@gmail.com>
2020-06-04 18:58:40 +0100

    bump to latest kaminari

--- lib/restpack_serializer.rb
+++ lib/restpack_serializer.rb
@@ -6,8 +6,6 @@ require 'restpack_serializer/serializable'
 require 'restpack_serializer/factory'
 require 'restpack_serializer/result'
 
-Kaminari::Hooks.init
-
 module RestPack
   module Serializer
     mattr_accessor :config
